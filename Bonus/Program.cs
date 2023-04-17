2ebc6dcbf3b90c6a310d4bae9e6ec6c3de2b6a01
stefanoreddd <127952037+stefanoreddd@users.noreply.github.com>
2023-04-17 18:14:55 +0200

    Delete Bonus directory

--- Bonus/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿/*
-BONUS:
-
-Aggiungere alla classe Rettangolo un metodo “disegna” che disegna in console il rettangolo con le sue dimensioni, ossia tanti “—” (due trattini) orizzontali quanto è grande la sua base e tanti ‘|’ verticali quanto e grande la sua altezza.
-*/
-
